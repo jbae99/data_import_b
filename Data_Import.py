cafa246b23ec0e525797ed5438d7b9827a3b7731
John Bae <jbae99@gmail.com>
2022-09-08 09:52:36 +0000

    Imported a data set and added data file to source

--- Data_Import.py
+++ Data_Import.py
@@ -1,5 +1,13 @@
-def main():
-  print('Hello World')
+from astropy.io import fits
 
+
+def main():
+  
+
+
+  hdul =   fits.open('cleanimage.G10.99.SiO.12m.7m.combined.image.mom0.fits')
+
+  hdul.info()
+  
 if __name__ == "__main__":
   main()
